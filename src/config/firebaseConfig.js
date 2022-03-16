b162a4186f99445c6e2fc354edddf0ab4ccdd13c
Andrey Kuznetsov <and.rey.q@yandex.ru>
2022-03-16 17:29:09 +0300

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDFI3aqrvYQ7r3-xdKxVkwb0iXPmfpk_0M",
+  authDomain: "akuz-chat.firebaseapp.com",
+  projectId: "akuz-chat",
+  storageBucket: "akuz-chat.appspot.com",
+  messagingSenderId: "695427114570",
+  appId: "1:695427114570:web:a9999b9c69caea3dbc28b3",
+  measurementId: "G-GXBDC0XK7H",
+};
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { UserContextProvider } from "./context/userContext";
+
+ReactDOM.render(
+  <UserContextProvider>
+    <App />
+  </UserContextProvider>,
+  document.getElementById("root")
+);
